e59b382627c3be01b8078e22ce08a6b007bd3cd8
mjo-at-github <132927667+mjo-at-github@users.noreply.github.com>
2024-03-31 19:03:43 +0200

    Migration to maven and JDK 21

--- src/main/java/JWeather.java
+++ src/main/java/JWeather.java
@@ -7,21 +7,23 @@ import com.google.gson.JsonParser;
 public class JWeather {
 
     private static double parseTemperatureFromJson(String json) {
-        JsonParser jsonParser = new JsonParser();
-        JsonObject jsonObject = jsonParser.parse(json).getAsJsonObject();
-        return jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+
+        JsonObject jsonParser = JsonParser.parseString(json).getAsJsonObject();
+
+        return jsonParser.getAsJsonObject("main").get("temp").getAsDouble();
+
     }
 
     public static void main(String[] args) {
 
         String apiKey = "e8afda2e61d4d6272f9313ee6637c5e5";
-        String city = "Berlin";
+        String city = "Cologne";
         String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(apiUrl)
                 .build();
-        try {
+        /*try {
             Response response = client.newCall(request).execute();
             if (response.isSuccessful()) {
                 String responseBody = response.body().string();
@@ -30,8 +32,22 @@ public class JWeather {
             } else {
                 System.out.println("Error: " + response.code() + " - " + response.message());
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } */
+
+        try (Response response = client.newCall(request).execute()){
+
+            if (response.isSuccessful()) {
+                assert response.body() != null;
+                String responseBody = response.body().string();
+                double temperature = parseTemperatureFromJson(responseBody);
+                System.out.println("Temperature in " + city + ": " + temperature + "°C");
+            } else {
+                System.out.println("Error: " + response.code() + " - " + response.message());
+            }
+        }
+
+        catch (Exception ignored) {
+
         }
     }
 }
