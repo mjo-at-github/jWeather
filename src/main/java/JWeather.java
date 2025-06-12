1c4f4bc5931ec937229123ef945e3e4c96742860
mjo-at-github <mjo>
2025-06-12 21:33:16 +0200

    JWeather updated

--- src/main/java/JWeather.java
+++ src/main/java/JWeather.java
@@ -7,17 +7,19 @@ import com.google.gson.JsonParser;
 public class JWeather {
 
     private static double parseTemperatureFromJson(String json) {
-
         JsonObject jsonParser = JsonParser.parseString(json).getAsJsonObject();
-
         return jsonParser.getAsJsonObject("main").get("temp").getAsDouble();
+    }
 
+    private static double parseFeelsLikeFromJson(String json) {
+        JsonObject jsonParser = JsonParser.parseString(json).getAsJsonObject();
+        return jsonParser.getAsJsonObject("main").get("feels_like").getAsDouble();
     }
 
     public static void main(String[] args) {
 
         String apiKey = "e8afda2e61d4d6272f9313ee6637c5e5";
-        String city = "Cologne";
+        String city = "Hamburg";
         String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
@@ -29,8 +31,12 @@ public class JWeather {
             if (response.isSuccessful()) {
                 assert response.body() != null;
                 String responseBody = response.body().string();
+
+                // System.out.println(responseBody);
+
                 double temperature = parseTemperatureFromJson(responseBody);
-                System.out.println("Temperature in " + city + ": " + temperature + "°C");
+                double feels_like = parseFeelsLikeFromJson(responseBody);
+                System.out.println("The temperature in " + city + " is " + temperature + "°C." + " Feels like " + feels_like + "°C.");
             } else {
                 System.out.println("Error: " + response.code() + " - " + response.message());
             }
